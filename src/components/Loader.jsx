8fd5c12bad72de0bb1495732fcd22ab8949012f2
Oleksandr <stambulyak1012@gmail.com>
2024-09-27 13:02:09 +0300

    custom loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import { ProgressBar } from 'react-loader-spinner';
 import './loader.css';
 
 export const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="loader">
-        <p>Loading...</p>
-      </div>
+      <ProgressBar
+        visible={true}
+        height="300"
+        width="300"
+        color="#4fa94d"
+        ariaLabel="progress-bar-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+      <p className="loader-text">Uploading Characters...</p>
     </div>
   );
 };
